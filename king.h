f4759fc09f9aee00444c5b0c3c16b038e64564a0
Kevin Trinh <ktrinh1@nd.edu>
2016-03-20 21:52:05 -0400

    more classes

---
+++ king.h
@@ -0,0 +1,63 @@
+// king.h
+//
+// Contains the King class
+
+#ifndef KING_H
+#define KING_H
+
+#include "piece.h"
+
+class King : public Piece {
+
+	public:
+		King(coord);
+		deque<coord> getPossMoves();
+	private:
+}
+
+King(coord nPosition) : Piece("King", nPosition) {
+}
+
+coord getPosition() {
+
+	// Initialize Variables
+	coord position = getPosition();
+	coord nCoord;
+	i = position[0];
+	j = position[1];
+	deque<coord> possMoves;
+
+	// Up
+	nCoord = [i, j+1];
+	possMove.push_back(nCoord);
+
+	// Down
+	nCoord = [i, j-1];
+	possMove.push_back(nCoord);
+
+	// Left
+	nCoord = [i-1, j];
+	possMove.push_back(nCoord);
+
+	// Right
+	nCoord = [i+1, j];
+	possMove.push_back(nCoord);
+
+	// Top-Left
+	nCoord = [i-1, j+1];
+	possMove.push_back(nCoord);
+
+	// Top-Right
+	nCoord = [i+1, j+1];
+	possMove.push_back(nCoord);
+
+	// Bottom-Left
+	nCoord = [i-1, j-1];
+	possMove.push_back(nCoord);
+
+	// Bottom-Right
+	nCoord = [i+1, j-1];
+	possMove.push_back(nCoord);
+
+	return possMoves;
+}
